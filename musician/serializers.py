ae55393654aae1ef24c46d375b55b43a0e8c32f7
Viesich <v.s.vesich@gmail.com>
2024-10-22 15:25:56 +0300

    fix flake8

--- musician/serializers.py
+++ musician/serializers.py
@@ -5,9 +5,18 @@ from musician.models import Musician
 class MusicianSerializer(serializers.ModelSerializer):
     class Meta:
         model = Musician
-        fields = ("first_name", "last_name", "instrument", "age", "date_of_applying", "is_adult")
+        fields = (
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult"
+        )
 
     def validate_age(self, value):
         if value < 14:
-            raise serializers.ValidationError("Musicians must be at least 14 years old.")
+            raise serializers.ValidationError(
+                "Musicians must be at least 14 years old."
+            )
         return value
